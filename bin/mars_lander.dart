ec5ed9c6be3473d3071714756b40e8a450bfc017
Dariusz Luksza <dariusz@luksza.org>
2023-07-04 21:49:13 +0100

    Initial commit

---
+++ bin/mars_lander.dart
@@ -0,0 +1,5 @@
+import 'package:mars_lander/mars_lander.dart' as mars_lander;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mars_lander.calculate()}!');
+}
---
+++ test/mars_lander_test.dart
@@ -0,0 +1,8 @@
+import 'package:mars_lander/mars_lander.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
